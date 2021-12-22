51115ab20b6ee1a606ae15060cf568a52d5faab3
mima <Michaela.Mattes@t-systems.com>
2021-12-22 13:53:34 +0100

    fix: update examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,47 +1,32 @@
-module "accounts-key_vault_secret" {
-  source           = "../modules/azure/terraform-accounts"
-  key_vault_secret = {
-    "mysecret" = {
-      key_vault_id = data.azurerm_key_vault.key_vault.id
-      value        = module.accounts-service_principal_password.service_principal_password["mysecret"].value
-      content_type = "sp password"
-    }
-  }
-}
-
-module "accounts-application" {
-  source      = "../modules/azure/terraform-accounts"
+module "accounts" {
+  source = "../modules/azure/terraform-accounts"
   application = {
-    aks = {}
+    aks-app = {
+      owners = data.azuread_group.grp-admin.members
+    }
   }
-}
-
-module "accounts-service_principal" {
-  source            = "../modules/azure/terraform-accounts"
   service_principal = {
-    aks = {
-      application_id = module.accounts-application.application.aks.application_id
-      description    = "aks service-principal"
+    /** service_principal for application */
+    aks-app = {
+      application_id = module.accounts.application.aks-app.application_id
+      description    = "service-principal for aks-app"
+      owners         = data.azuread_group.grp-admin.members
     }
   }
-}
-module "accounts-service_principal_password" {
-  source                     = "../modules/azure/terraform-accounts"
   service_principal_password = {
-    aks = {
-      service_principal_id = module.accounts-service_principal.service_principal.aks.object_id
-      rotation             = time_rotating.rotating.aks.id
+    aks-app = {
+      service_principal_id = module.accounts.service_principal.aks-app.object_id
+      rotation             = time_rotating.rotating["service_principal"].id
     }
   }
-}
-
-module "accounts-role_assignment" {
-  source          = "../modules/azure/terraform-accounts"
-  role_assignment = {
-    aks = {
-      scope                = data.azurerm_container_registry.container_registry.id
-      role_definition_name = "acrpull"
-      principal_id         = module.accounts-service_principal.service_principal.aks.object_id
+  key_vault_secret = {
+    aks-app = {
+      key_vault_id = data.azurerm_key_vault.key_vault_environment.id
+      value        = module.accounts.service_principal_password.aks-app.value
+      content_type = local.service_principal.aks-app.description
     }
+  }
+  tags = {
+    service = "service_name"
   }
 }
