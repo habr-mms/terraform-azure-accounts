cfacca73fe208f02ec91c8ad588430253861d5d6
mima <Michaela.Mattes@t-systems.com>
2022-04-20 18:42:45 +0200

    change versions dependencies

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = "~>2.19"
+      version = ">=2.19"
     }
     azuread = {
       source  = "registry.terraform.io/hashicorp/azuread"
-      version = "~>2.5"
+      version = ">=2.5"
     }
   }
   required_version = "~>1.0"
